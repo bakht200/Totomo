8ef5d3d876d83ce799222446dd8cb3e1b8e6f09f
bakht200 <62112170+bakht200@users.noreply.github.com>
2022-09-10 23:22:01 +0500

    profile page design updated

--- lib/view/profile.dart
+++ lib/view/profile.dart
@@ -3,43 +3,198 @@ import 'package:dating_app/view/edit_profile.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  List interestList = [
+    {'title': 'Movies', 'image': 'assets/images/love.png'},
+    {'title': 'Cooking', 'image': 'assets/images/cup.png'},
+    {'title': 'Entertaintment', 'image': 'assets/images/businessman.png'},
+    {'title': 'Game', 'image': 'assets/images/love.png'},
+  ];
+
+  List<int> selectedItems = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: AppBar(
-        automaticallyImplyLeading: false,
-        elevation: 0,
-        backgroundColor: Color(AppTheme.appBarBackgroundColor),
-        title: Text(
-          "Profile",
-          style: TextStyle(
-            color: white,
-            fontWeight: FontWeight.bold,
-            fontSize: 22.sp,
-          ),
-        ),
-      ),
-      body: const CustomScrollView(
-        slivers: [
-          SliverToBoxAdapter(
-            child: _ProfileHeader(
-              numberOfPosts: 0,
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          elevation: 0,
+          backgroundColor: Color(AppTheme.appBarBackgroundColor),
+          title: Text(
+            "Profile",
+            style: TextStyle(
+              color: white,
+              fontWeight: FontWeight.bold,
+              fontSize: 22.sp,
             ),
           ),
-          SliverToBoxAdapter(
-            child: _EditProfileButton(),
+          actions: [
+            Padding(
+              padding: EdgeInsets.only(right: 8.0.w),
+              child: GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).push(MaterialPageRoute(
+                        builder: (builder) => EditProfilePage()));
+                  },
+                  child: Icon(
+                    Icons.edit,
+                    color: Colors.white,
+                  )),
+            ),
+          ],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 150.h,
+                width: MediaQuery.of(context).size.width,
+                child: Image.asset(
+                  'assets/images/googlemap.png',
+                  fit: BoxFit.fitWidth,
+                ),
+              ),
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: CircleAvatar(
+                      radius: 30.r,
+                      backgroundImage: NetworkImage(
+                          'https://cdn-icons-png.flaticon.com/512/149/149071.png'),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            Text(
+                              'Sophia',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 17.0.sp,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            Icon(
+                              Icons.diamond,
+                              color: Colors.amber,
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              _EditProfileButton(),
+              Padding(
+                padding: EdgeInsets.only(left: 8.0.w),
+                child: Row(
+                  children: [
+                    Text(
+                      'Interest',
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 17.0.sp,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 140.h,
+                child: GridView.count(
+                  physics: BouncingScrollPhysics(),
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 3.0,
+                  mainAxisSpacing: 5.0,
+                  childAspectRatio: (1 / .35),
+                  shrinkWrap: true,
+                  children: List.generate(
+                    interestList.length,
+                    (index) {
+                      return Padding(
+                          padding: EdgeInsets.all(7.0.w),
+                          child: GestureDetector(
+                            onTap: () {
+                              // setState(() {
+                              //   if (selectedItems.contains(index)) {
+                              //     selectedItems.remove(index);
+                              //   } else {
+                              //     selectedItems.add(index);
+                              //   }
+                              // });
+                            },
+                            child: Container(
+                              height: 20.h,
+                              width: 20.w,
+                              child: Center(
+                                child: Row(
+                                  children: [
+                                    Image.asset(
+                                      interestList[index]['image'],
+                                      height: 25.h,
+                                    ),
+                                    SizedBox(
+                                      width: 10.w,
+                                    ),
+                                    Text(
+                                      interestList[index]['title'],
+                                      style: TextStyle(
+                                        fontSize: 14.sp,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                border: Border.all(
+                                  width: 2,
+                                  color: selectedItems.contains(index)
+                                      ? Color(AppTheme.primaryColor)
+                                      : Colors.white,
+                                ),
+                                borderRadius: BorderRadius.only(
+                                    topLeft: Radius.circular(10.r),
+                                    topRight: Radius.circular(10.r),
+                                    bottomLeft: Radius.circular(10.r),
+                                    bottomRight: Radius.circular(10.r)),
+                                boxShadow: [
+                                  BoxShadow(
+                                    color: Color(AppTheme.primaryColor)
+                                        .withOpacity(0.5),
+                                    spreadRadius: 5,
+                                    blurRadius: 7,
+                                    offset: Offset(0, 3),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ));
+                    },
+                  ),
+                ),
+              ),
+              _NoPostsMessage(),
+            ],
           ),
-          SliverToBoxAdapter(
-            child: SizedBox(height: 24),
-          ),
-          SliverFillRemaining(child: _NoPostsMessage())
-        ],
-      ),
-    );
+        ));
   }
 }
 
@@ -48,27 +203,60 @@ class _EditProfileButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.symmetric(horizontal: 8.0.w),
-      child: Container(
-        decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(6.r),
-            border: Border.all(color: Color(AppTheme.appBarBackgroundColor))),
-        child: OutlinedButton(
-          onPressed: () {
-            Navigator.of(context).push(
-                MaterialPageRoute(builder: (builder) => EditProfilePage()));
-          },
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: EdgeInsets.only(left: 8.0.w),
           child: Text(
-            'Edit Profile',
+            'Self Introduction',
             style: TextStyle(
-              color: Color(AppTheme.primaryColor),
-              fontSize: 14.0.sp,
+              color: Colors.black,
+              fontSize: 17.0.sp,
               fontWeight: FontWeight.bold,
             ),
           ),
         ),
-      ),
+        Container(
+          height: 70.h,
+          width: 500.w,
+          child: Container(
+              margin: EdgeInsets.all(12.w),
+              child: Text(
+                'Hi i am sophi i am from china i am learning spanish feel free to talk to me Hi i am sophi i am from china i am learning spanish feel free to talk to me',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12.0.sp,
+                  fontWeight: FontWeight.normal,
+                ),
+              )),
+        ),
+        // Padding(
+        //   padding: EdgeInsets.symmetric(horizontal: 8.0.w),
+        //   child: Container(
+        //     height: 40.h,
+        //     width: 450.w,
+        //     decoration: BoxDecoration(
+        //         borderRadius: BorderRadius.circular(6.r),
+        //         border:
+        //             Border.all(color: Color(AppTheme.appBarBackgroundColor))),
+        //     child: OutlinedButton(
+        //       onPressed: () {
+        //         Navigator.of(context).push(
+        //             MaterialPageRoute(builder: (builder) => EditProfilePage()));
+        //       },
+        //       child: Text(
+        //         'Edit Profile',
+        //         style: TextStyle(
+        //           color: Color(AppTheme.primaryColor),
+        //           fontSize: 14.0.sp,
+        //           fontWeight: FontWeight.bold,
+        //         ),
+        //       ),
+        //     ),
+        //   ),
+        // ),
+      ],
     );
   }
 }
@@ -150,25 +338,8 @@ class _ProfileHeader extends StatelessWidget {
                 ),
                 Padding(
                   padding: _statitisticsPadding,
-                  child: Column(
-                    children: [
-                      Text(
-                        '15',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12.0.sp,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                      Text(
-                        'Premium Users',
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 12.0.sp,
-                          fontWeight: FontWeight.normal,
-                        ),
-                      ),
-                    ],
+                  child: SizedBox(
+                    width: 40.w,
                   ),
                 ),
               ],
@@ -176,23 +347,80 @@ class _ProfileHeader extends StatelessWidget {
           ],
         ),
         Align(
-          alignment: Alignment.centerLeft,
           child: Padding(
-            padding: const EdgeInsets.all(8.0),
+            padding: EdgeInsets.all(8.0.w),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Text(
-                  "Full name",
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16.0.sp,
-                    fontWeight: FontWeight.w500,
+                Padding(
+                  padding: EdgeInsets.all(8.0.w),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Full name",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Josh',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0.sp,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
                   ),
                 ),
-                Icon(
-                  Icons.star,
-                  color: Colors.amber,
-                )
+                Padding(
+                  padding: EdgeInsets.all(8.0.w),
+                  child: Column(
+                    children: [
+                      Text(
+                        "Country",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Japan',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0.sp,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(8.0.w),
+                  child: Column(
+                    children: [
+                      Text(
+                        "City",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16.0.sp,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      Text(
+                        'Tokyo',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 14.0.sp,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ],
             ),
           ),
@@ -209,10 +437,20 @@ class _NoPostsMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(children: [
-      Expanded(
-        flex: 5,
-        child: Container(
+    return SingleChildScrollView(
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        Padding(
+          padding: EdgeInsets.only(left: 8.0.w),
+          child: Text(
+            'Recent Posts',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 17.0.sp,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        Container(
           decoration: BoxDecoration(border: Border.all(color: Colors.black38)),
           child: Wrap(
               spacing: 2,
@@ -220,7 +458,7 @@ class _NoPostsMessage extends StatelessWidget {
               children: List.generate(12, (index) {
                 return Container(
                   width: 70.w,
-                  height: 200.h,
+                  height: 100.h,
                   decoration: BoxDecoration(
                       image: DecorationImage(
                           image: NetworkImage(
@@ -229,8 +467,8 @@ class _NoPostsMessage extends StatelessWidget {
                           fit: BoxFit.cover)),
                 );
               })),
-        ),
-      )
-    ]);
+        )
+      ]),
+    );
   }
 }
